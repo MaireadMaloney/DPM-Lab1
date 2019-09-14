7448d0eed457d216ec2d616103c0f1ce70c8a20e
Mairead Maloney <mairead.maloney@mail.mcgill.ca>
2019-09-14 15:53:33 -0400

    starting p controller

--- src/ca/mcgill/ecse211/lab1/PController.java
+++ src/ca/mcgill/ecse211/lab1/PController.java
@@ -6,8 +6,7 @@ public class PController extends UltrasonicController {
 
   public static int distError=0; // Error (amount to close or too far in meters
   public static final int DEADBAND = 2; // Error threshold
-  public static final int FWDSPEED = 200; // Default rotational speed of wheels
-  public static final int DELTASPD = 10; // Bang-bang constant
+  public static final int FWDSPEED = 150; // Default rotational speed of wheels
   public static final int SLEEPINT = 50; // Sleep interval 50 mS = 20Hz
   
   public PController() {
@@ -20,28 +19,28 @@ public class PController extends UltrasonicController {
   @Override
   public void processUSData(int distance) {
     filter(distance);
+    int deltaspeed = 2;
     
     distError=distance - BAND_CENTER; // Compute error
     
-    if (Math.abs(distError) <= BAND_WIDTH) { // Within limits, same speed
-    LEFT_MOTOR.setSpeed(FWDSPEED); // Start moving forward
-    RIGHT_MOTOR.setSpeed(FWDSPEED);
-    LEFT_MOTOR.forward();
-    RIGHT_MOTOR.forward();
+    if(BAND_WIDTH > Math.abs(distError)) {
+      LEFT_MOTOR.setSpeed(FWDSPEED); // Start moving forward
+      RIGHT_MOTOR.setSpeed(FWDSPEED);
+    }
+    
+    else if (distError < 0) { //Too far from the wall
+    LEFT_MOTOR.setSpeed(FWDSPEED+(deltaspeed*distError));
+    RIGHT_MOTOR.setSpeed(FWDSPEED-(deltaspeed*distError));
+    //LEFT_MOTOR.forward();
+    //RIGHT_MOTOR.forward();
     }
     
     else if (distError > 0) { // Too close from the wall
-    LEFT_MOTOR.setSpeed(FWDSPEED);
-    RIGHT_MOTOR.setSpeed(FWDSPEED-(DELTASPD*distError));
-    LEFT_MOTOR.forward();
-    RIGHT_MOTOR.forward();
-    }
-    else if (distError < 0) { //Too far from the wall
-    LEFT_MOTOR.setSpeed(FWDSPEED);
-    RIGHT_MOTOR.setSpeed(FWDSPEED+(DELTASPD*distError));
-    LEFT_MOTOR.forward();
-    RIGHT_MOTOR.forward();
-    }
+      LEFT_MOTOR.setSpeed(FWDSPEED+(deltaspeed*distError));
+      RIGHT_MOTOR.setSpeed(FWDSPEED-(deltaspeed*distError));
+      //LEFT_MOTOR.forward();
+      //RIGHT_MOTOR.forward();
+      }
 
 
     // TODO: process a movement based on the us distance passed in (P style)
