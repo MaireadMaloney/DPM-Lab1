c9a440f192ca66fe7e2a44f5139256d56be5e401
jbelanger25 <jonathan.belanger2@mail.mcgill.ca>
2019-09-15 13:15:43 -0400

    Tweaking code

--- src/ca/mcgill/ecse211/lab1/PController.java
+++ src/ca/mcgill/ecse211/lab1/PController.java
@@ -6,7 +6,7 @@ public class PController extends UltrasonicController {
 
   public static int distError=0; // Error (amount to close or too far in meters
   public static final int FWDSPEED = 100; // Default rotational speed of wheels
-  public static final int BAND = 45;
+  public static final int BAND = 35;
   public static final int BAND_W = 5;
   
   
@@ -21,39 +21,28 @@ public class PController extends UltrasonicController {
   public void processUSData(int distance) {
     filter(distance);
     int deltaSpeedSlow = 2;
-    int deltaSpeedFast = 4;
+    int deltaSpeedFast = 6;
     distError=distance - BAND; // Compute error
     int speedChangeSlow = Math.abs(deltaSpeedSlow * distError);
     int speedChangeFast = Math.abs(deltaSpeedFast * distError);
     
     if(BAND_W >= Math.abs(distError)) {
       LEFT_MOTOR.setSpeed(FWDSPEED);// Start moving forward
-      LEFT_MOTOR.forward();
       RIGHT_MOTOR.setSpeed(FWDSPEED);
       System.out.println("Striaght " + distance );
     }
-    
-    else if (distError > 0) { //Too far from the wall, change wheel speeds based on magnitude of error
-      if(distError > 140) {
-        LEFT_MOTOR.setSpeed(50);
-        RIGHT_MOTOR.setSpeed(50);
-        RIGHT_MOTOR.backward();
-      }
-    RIGHT_MOTOR.forward();
-    LEFT_MOTOR.setSpeed(FWDSPEED+(speedChangeSlow + 30));
-    LEFT_MOTOR.forward();
-    RIGHT_MOTOR.setSpeed(FWDSPEED-(speedChangeFast-30));
+    else if (distError > 0 && distError != 2147483612 ) { //Too far from the wall, change wheel speeds based on magnitude of error
+    LEFT_MOTOR.setSpeed(FWDSPEED+(speedChangeSlow));
+    RIGHT_MOTOR.setSpeed(FWDSPEED-(speedChangeFast));
     System.out.println("Right " + distance);
     System.out.println(distError);
     }
-    
+    else if (distError < -15) {
+      RIGHT_MOTOR.setSpeed(75);
+      LEFT_MOTOR.setSpeed(2);
+      System.out.println(distError + "LEFT");
+    }
     else if (distError < 0) { // Too close to the wall, change wheel speeds based on magnitude of error
-      if(distError < -25) {
-        RIGHT_MOTOR.setSpeed(50);
-        LEFT_MOTOR.setSpeed(50);
-        LEFT_MOTOR.backward();
-      }
-      LEFT_MOTOR.forward();
       RIGHT_MOTOR.setSpeed(FWDSPEED+(speedChangeFast));
       LEFT_MOTOR.setSpeed(FWDSPEED-(speedChangeSlow));
       System.out.println("Left " + distance );
