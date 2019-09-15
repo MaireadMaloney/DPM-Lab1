d5f737b98905f6880f414feca5f75e427156240e
jbelanger25 <jonathan.belanger2@mail.mcgill.ca>
2019-09-15 14:06:02 -0400

    Still not working but getting there

--- src/ca/mcgill/ecse211/lab1/PController.java
+++ src/ca/mcgill/ecse211/lab1/PController.java
@@ -5,7 +5,7 @@ import static ca.mcgill.ecse211.lab1.Resources.*;
 public class PController extends UltrasonicController {
 
   public static int distError=0; // Error (amount to close or too far in meters
-  public static final int FWDSPEED = 100; // Default rotational speed of wheels
+  public static final int FWDSPEED = 200; // Default rotational speed of wheels
   public static final int BAND = 35;
   public static final int BAND_W = 5;
   
@@ -20,31 +20,36 @@ public class PController extends UltrasonicController {
   @Override
   public void processUSData(int distance) {
     filter(distance);
-    int deltaSpeedSlow = 2;
-    int deltaSpeedFast = 6;
+    int deltaSpeedSlow = 1;
+    int deltaSpeedFast = 4;
     distError=distance - BAND; // Compute error
     int speedChangeSlow = Math.abs(deltaSpeedSlow * distError);
     int speedChangeFast = Math.abs(deltaSpeedFast * distError);
     
+    if (distError == 2147483647) { //handles bad reading
+      distError = -25;
+    }
     if(BAND_W >= Math.abs(distError)) {
       LEFT_MOTOR.setSpeed(FWDSPEED);// Start moving forward
       RIGHT_MOTOR.setSpeed(FWDSPEED);
+      RIGHT_MOTOR.forward();
+      LEFT_MOTOR.forward();
       System.out.println("Striaght " + distance );
     }
-    else if (distError > 0 && distError != 2147483612 ) { //Too far from the wall, change wheel speeds based on magnitude of error
+    else if (distError > 0) { //Too far from the wall, change wheel speeds based on magnitude of error
+      if (distError > 70) { //Putting cap on speed change
+        distError = 70;
+        speedChangeSlow = deltaSpeedSlow * distError;
+        speedChangeFast = deltaSpeedFast * distError;
+      }
     LEFT_MOTOR.setSpeed(FWDSPEED+(speedChangeSlow));
     RIGHT_MOTOR.setSpeed(FWDSPEED-(speedChangeFast));
     System.out.println("Right " + distance);
     System.out.println(distError);
     }
-    else if (distError < -15) {
-      RIGHT_MOTOR.setSpeed(75);
-      LEFT_MOTOR.setSpeed(2);
-      System.out.println(distError + "LEFT");
-    }
     else if (distError < 0) { // Too close to the wall, change wheel speeds based on magnitude of error
       RIGHT_MOTOR.setSpeed(FWDSPEED+(speedChangeFast));
-      LEFT_MOTOR.setSpeed(FWDSPEED-(speedChangeSlow));
+      LEFT_MOTOR.setSpeed(FWDSPEED-(speedChangeFast));
       System.out.println("Left " + distance );
       System.out.println(distError);
       }
--- src/ca/mcgill/ecse211/lab1/UltrasonicPoller.java
+++ src/ca/mcgill/ecse211/lab1/UltrasonicPoller.java
@@ -34,7 +34,7 @@ public class UltrasonicPoller implements Runnable {
       distance = (int) (usData[0] * 100.0); // extract from buffer, convert to cm, cast to int
       controller.processUSData(distance); // now take action depending on value
       try {
-        Thread.sleep(80);
+        Thread.sleep(50);
       } catch (Exception e) {
       } // Poor man's timed sampling
     }
